0d4f262cfb920beaf6be6e231efa17c8b507ee29
shivansh_max <shivanshupadhyay22@gmail.com>
2022-10-25 21:46:21 -0500

    Updated for remote development and practice problems.

---
+++ challenges/SubarayWithGivenSum.py
@@ -0,0 +1,7 @@
+class Solution:
+    def subArraySum(self, arr, n, s):
+        for index, value in enumerate(arr):
+            print(value)
+
+
+Solution().subArraySum([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 10, 15)
